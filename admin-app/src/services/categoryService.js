034d6ec2857a215667c7c5acf8392309823fd08f
Ľubomír Grňo <lgrno@trask.cz>
2026-01-18 12:20:33 +0100

    fix categories

--- admin-app/src/services/categoryService.js
+++ admin-app/src/services/categoryService.js
@@ -2,13 +2,12 @@ import { authFetch } from '../utils/authFetch';
 
 const API_URL = `${process.env.REACT_APP_API_URL}/categories`;
 
-export const getCategories = async (request = {}) => {
+export const getCategories = async () => {
   const response = await authFetch(API_URL, {
-    method: 'POST',
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(request),
+    }
   });
   return response.json();
 };
